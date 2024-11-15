522924fdc2fb2103d1389baccfa463d5bc10074f
blejj <fbleileee@gmail.com>
2024-11-14 21:52:33 -0300

    Correcciones, agregué nuevo genero, acceso a pdf, etc.

--- src/script/script.js
+++ src/script/script.js
@@ -4,17 +4,3 @@ const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 3000,
   touch: false
 })
-
-
-//gestion de clicks en los comics card
-document.addEventListener("DOMContentLoaded",() => {
-  const cards = document.querySelectorAll(".card");
-
-  cards.forEach(card => {
-    card.addEventListener("click", () => {
-      const pdfUrl = card.getAttribute("data-pdf");
-
-      window.open(pdfUrl, "_blank");
-    })
-  })
-})
