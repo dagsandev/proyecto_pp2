17c0570cc928ed6af57fb9ddd83f8146f6f912a9
Santiago <santidaglio@gmail.com>
2024-11-27 02:27:27 -0300

    se añade servicio de comics

---
+++ backend/registerloginapp/Services/ComicService.cs
@@ -0,0 +1,35 @@
+﻿using RegisterLoginApp.Models;
+using RegisterLoginApp.Models.Dto;
+using RegisterLoginApp.Repository;
+
+namespace RegisterLoginApp.Services
+{
+    public class ComicService : IComicService
+    {
+        private readonly IComicRepository _comicRepository;
+
+        public ComicService(IComicRepository comicRepository)
+        {
+            _comicRepository = comicRepository;
+        }
+
+        public async Task AddComic(ComicModel comic)
+        {
+            await _comicRepository.AddComic(comic);
+        }
+
+        public List<ComicResponseDto> FindAll()
+        {
+            var comics = _comicRepository.FindAll().Result;
+
+            var comicDto = comics.Select(comic => new ComicResponseDto
+            {
+                Title = comic.Title,
+                Genre = comic.Genre,
+                File = comic.FilePath
+            }).ToList();
+
+            return comicDto;
+        }
+    }
+}
---
+++ backend/registerloginapp/Services/IComicService.cs
@@ -0,0 +1,11 @@
+﻿using RegisterLoginApp.Models;
+using RegisterLoginApp.Models.Dto;
+
+namespace RegisterLoginApp.Services
+{
+    public interface IComicService
+    {
+        Task AddComic(ComicModel comic);
+        List<ComicResponseDto> FindAll();
+    }
+}
