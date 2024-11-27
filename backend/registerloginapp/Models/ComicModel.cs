32f653fb964294ee62e5ce97179757ce5c2fa472
Santiago <santidaglio@gmail.com>
2024-11-27 02:31:34 -0300

    se añade comic model

---
+++ backend/registerloginapp/Models/ComicModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RegisterLoginApp.Models
+{
+    public class ComicModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string FilePath { get; set; }
+    }
+}
