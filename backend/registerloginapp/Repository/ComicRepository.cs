960d7c73bc628e67072bae779b49468b35b44d41
Santiago <santidaglio@gmail.com>
2024-11-27 02:29:55 -0300

    se añade repositorio de comics

---
+++ backend/registerloginapp/Repository/ComicRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegisterLoginApp.Data;
+using RegisterLoginApp.Models;
+
+namespace RegisterLoginApp.Repository
+{
+    public class ComicRepository : IComicRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ComicRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddComic(ComicModel comic)
+        {
+            _context.Comics.Add(comic);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<ComicModel>> FindAll()
+        {
+            return await _context.Comics.ToListAsync();
+        }
+
+        public async Task<ComicModel> GetComicById(int id)
+        {
+            return await _context.Comics.FirstOrDefaultAsync(c => c.Id == id);
+        }
+    }
+}
---
+++ backend/registerloginapp/Repository/IComicRepository.cs
@@ -0,0 +1,11 @@
+﻿using RegisterLoginApp.Models;
+
+namespace RegisterLoginApp.Repository
+{
+    public interface IComicRepository
+    {
+        Task AddComic(ComicModel comic);
+        Task<List<ComicModel>> FindAll();
+        Task<ComicModel> GetComicById(int id);
+    }
+}
